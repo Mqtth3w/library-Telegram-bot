3682b56d54b2663dca24c80b6f431bd4a9a1c68b
Matteo Gianvenuti <mqtth3w@duck.com>
2025-03-05 20:53:57 +0100

    Add files via upload

--- lib_tel_bot.js
+++ lib_tel_bot.js
@@ -7,19 +7,33 @@
  */
 
 // userid allowed to run edit commands (an admin is also a user)
-admins = [123456789];
+const admins = ["123456789"];
 //userid allowed to run read-only commands
-users = [1265456];
+const users = ["1265456"];
 
+/**
+ * Sends a text message to a specified user via a Telegram bot.
+ *
+ * @param {object} env - The environment object containing runtime information, such as bindings.
+ * @param {number|string} chatId - The chat ID of the user who requested the service.
+ * @param {string} text - The message to send.
+ * @returns {Promise<void>} - This function does not return a value.
+ */
 async function sendMessage(env, chatId, text) {
     const url = `https://api.telegram.org/bot${env.API_KEY}/sendMessage`;
-    return fetch(url, {
+    await fetch(url, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ chat_id: chatId, text })
     });
 }
 
+/** 
+ * Search for a book through ISBN and Google books API.
+ * 
+ * @param {string} isbn - The book ISBN.
+ * @returns {Promise<object|null>} The book data.
+ */
 async function fetchBookData(isbn) {
     const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=isbn:${isbn}`);
     const data = await response.json();
@@ -40,28 +54,216 @@ async function fetchBookData(isbn) {
     };
 }
 
+/** 
+ * convert a ISBN10 to ISBN13.
+ * 
+ * @param {string} isbn10 - The book ISBN10.
+ * @returns {Promise<string>} The ISBN13.
+ */
+async function convertISBN10toISBN13(isbn10) {
+    let isbn13Base = "978" + isbn10.slice(0, 9);
+    let sum = 0;
+    for (let i = 0; i < isbn13Base.length; i++) {
+        sum += parseInt(isbn13Base[i]) * (i % 2 === 0 ? 1 : 3);
+    }
+    let checkDigit = (10 - (sum % 10)) % 10;
+    return isbn13Base + checkDigit;
+}
+
+/** 
+ * Add a book to the DB by ISBN taking its data online.
+ * 
+ * @param {object} env - The environment object containing runtime information, such as bindings.
+ * @param {number|string} chatId - The chat ID of the user who requested the service.
+ * @param {string} isbn - The book ISBN.
+ * @returns {Promise<void>} This function does not return a value.
+ */
 async function addBook(env, chatId, isbn) {
-    const book = await fetchBookData(isbn);
-    if (!book) return await sendMessage(env, chatId, `Book ${isbn} not found.`);
-    await env.db.prepare("INSERT INTO books VALUES (?,?,?,?,?,?,?,?,?,?,?,?)")
-		.bind(book.isbn10, book.isbn13, book.title, book.authors, book.publisher, book.publishedDate, 
-			book.pageCount, book.textSnippet, book.description, book.language, "", book.thumbnail).run();
-    message += `The following book has been found and added\n` +
-			`isbn10: ${book.isbn10}\n` +
-			`isbn13: ${book.isbn13}\n` +
+	if (isbn && Number(isbn) > 0 && (isbn.length === 10 || isbn.length === 13)) {
+		const book = await fetchBookData(isbn);
+		if (!book) return await sendMessage(env, chatId, `Book ${isbn} not found.`);
+		await env.db.prepare("INSERT INTO books VALUES (?,?,?,?,?,?,?,?,?,?,?,?)")
+			.bind(book.isbn10, book.isbn13, book.title, book.authors, book.publisher, book.publishedDate, 
+				book.pageCount, book.textSnippet, book.description, book.language, "", book.thumbnail).run();
+		let message = `The following book has been found and added\n` +
+				`ISBN10: ${book.isbn10}\n` +
+				`ISBN13: ${book.isbn13}\n` +
+				`title: ${book.title}\n` +
+				`authors: ${book.authors}\n` +
+				`publisher: ${book.publisher}\n` +
+				`publishedDate: ${book.publishedDate}\n` +
+				`pageCount: ${book.pageCount}\n` +
+				`textSnippet: ${book.textSnippet}\n` + 
+				`description: ${book.description}\n` +
+				`language: ${book.language}\n` +
+				`location: \n` +
+				`thumbnail: ${book.thumbnail}\n`;
+		await sendMessage(env, chatId, message);
+	}
+	else await sendMessage(env, chatId, "Use a ISBN10 or ISBN13 number.");
+}
+
+/** 
+ * Delete a book by the specified ISBN from the DB.
+ * 
+ * @param {object} env - The environment object containing runtime information, such as bindings.
+ * @param {number|string} chatId - The chat ID of the user who requested the service.
+ * @param {string} isbn - The book ISBN.
+ * @returns {Promise<void>} This function does not return a value.
+ */
+async function deleteBook(env, chatId, isbn) {
+    if (isbn && Number(isbn) > 0 && (isbn.length === 10 || isbn.length === 13)) {
+		await env.db.prepare(`DELETE FROM books WHERE ${isbn.length === 10 ? "isbn10" : "isbn13"} = ? LIMIT 1`).bind(isbn).run();
+		await sendMessage(env, chatId, `Book ${isbn} deleted (if exists).`);
+	}
+	else await sendMessage(env, chatId, "Use a ISBN10 or ISBN13 number.");
+}
+
+/** 
+ * Add a book with the manually inserted data in the DB.
+ * 
+ * @param {object} env - The environment object containing runtime information, such as bindings.
+ * @param {number|string} chatId - The chat ID of the user who requested the service.
+ * @param {string} args - The book data.
+ * @returns {Promise<void>} This function does not return a value.
+ */
+async function addManually(env, chatId, args) {
+    const [isbn10, isbn13, title, authors, publisher, publishedDate, pageCount, textSnippet, description, language, location, thumbnail] = args.split(";");
+    if ((isbn10 && Number(isbn10) > 0 && isbn10.length === 10) || (isbn13 && Number(isbn13) > 0 && isbn13.length === 10)) {
+		let pages = Number(pageCount);
+		if (isNaN(pages) || pages <= 0) pages = 1;
+		await env.db.prepare("INSERT INTO books VALUES (?,?,?,?,?,?,?,?,?,?,?,?)")
+		.bind(isbn10 || "", isbn13 || convertISBN10toISBN13(isbn10), title || "", authors || "", publisher || "", publishedDate || "", 
+			pages, textSnippet || "", description || "", language || "", location || "", thumbnail || "").run();
+		await sendMessage(env, chatId, `Book ISBN10 ${isbn10}, ISBN13 ${isbn13} added.`);
+	} else await sendMessage(env, chatId, "You must provide a valid ISBN10 or ISBN13.");
+}
+
+/** 
+ * Update a specified filed of a specified book.
+ * 
+ * @param {object} env - The environment object containing runtime information, such as bindings.
+ * @param {number|string} chatId - The chat ID of the user who requested the service.
+ * @param {string} command - Indicate the filed to be updated.
+ * @param {object} args - The new field of the field.
+ * @returns {Promise<void>} This function does not return a value.
+ */
+async function updateBook(env, chatId, command, args) {
+    let [isbn, newValue] = args.split(" ", 2);
+    if (isbn && Number(isbn) > 0 && (isbn.length === 10 || isbn.length === 13) && newValue) {
+		const fieldMap = {
+			"/settitle": "title",
+			"/setauthors": "authors",
+			"/setpublisher": "publisher",
+			"/setdate": "publishedDate",
+			"/setpages": "pageCount",
+			"/setsnippet": "textSnippet",
+			"/setdesc": "description",
+			"/setlang": "language",
+			"/setlocation": "location"
+		};
+		if (command === "/setpages") {
+			newValue = Number(newValue);
+			if (isNaN(newValue) || newValue <= 0) {
+				return await sendMessage(env, chatId, "Invalid page count. It must be a positive number.");
+			}
+		}
+		await env.db.prepare(`UPDATE books SET ${fieldMap[command]} = ? WHERE ${isbn.length === 10 ? "isbn10" : "isbn13"} = ? LIMIT 1`)
+			.bind(newValue, isbn).run();
+		await sendMessage(env, chatId, "If the book exists then it has been updated.");
+    }
+	else await sendMessage(env, chatId, "You must provide a valid ISBN10 or ISBN13, and a new value.");
+}
+
+/** 
+ * Search for all books that match the title.
+ * 
+ * @param {object} env - The environment object containing runtime information, such as bindings.
+ * @param {number|string} chatId - The chat ID of the user who requested the service.
+ * @param {number|string} title - The book's title to search.
+ * @returns {Promise<void>} This function does not return a value.
+ */
+async function searchBooks(env, chatId, title) {
+    const { results } = await env.db.prepare("SELECT ISBN10, ISBN13, title, authors, publisher, publishedDate FROM books WHERE title LIKE ?")
+								.bind(`%${title}%`).all();
+    if (results.length === 0) return await sendMessage(env, chatId, `No title contains ${title}.`);
+    let total = 0;
+	let message = "";
+	const batchSize = 25;
+	for (let i = 0; i < results.length; i++) {
+		const book = results[i];
+		total++;
+		message += `ISBN10: ${book.isbn10}\n` +
+			`ISBN13: ${book.isbn13}\n` +
             `title: ${book.title}\n` +
             `authors: ${book.authors}\n` +
             `publisher: ${book.publisher}\n` +
-            `publishedDate: ${book.publishedDate}\n` +
-			`pageCount: ${book.pageCount}\n` +
-			`textSnippet: ${book.textSnippet}\n` + 
-			`description: ${book.description}\n` +
-			`language: ${book.language}\n` +
-			`thumbnail: ${book.thumbnail}\n`;
-	return await sendMessage(env, chatId, message);
+            `publishedDate: ${book.publishedDate}\n\n`;
+		if ((total % batchSize === 0) || (i === results.length - 1)) {
+			if (i === results.length - 1) {
+				message += `total books matched: ${total}.`;
+			}
+			await sendMessage(env, chatId, message);
+			await new Promise(resolve => setTimeout(resolve, 30));
+			message = ""; 
+		}
+    }
 }
 
-/**
+/** 
+ * Shows all the data about a specific book found by ISBN.
+ * 
+ * @param {object} env - The environment object containing runtime information, such as bindings.
+ * @param {number|string} chatId - The chat ID of the user who requested the service.
+ * @param {string} isbn - The book ISBN10 or ISBN13.
+ * @returns {Promise<void>} This function does not return a value.
+ */
+async function showBook(env, chatId, isbn) {
+	if (isbn && Number(isbn) > 0 && (isbn.length === 10 || isbn.length === 13)) {
+		const { results } = await env.db.prepare(`SELECT * FROM books WHERE ${isbn.length === 10 ? "isbn10" : "isbn13"} = ?`)
+									.bind(isbn).all();
+		if (results.length === 0) return await sendMessage(env, chatId, `No books found with ${isbn.length === 10 ? "ISBN10" : "ISBN13"} ${isbn}.`);
+		let message = "";
+		let total = 0;
+		const batchSize = 1;
+		for (let i = 0; i < results.length; i++) {
+			const book = results[i];
+			total++;
+			message += `The following book has been found\n` +
+				`ISBN10: ${book.isbn10}\n` +
+				`ISBN13: ${book.isbn13}\n` +
+				`title: ${book.title}\n` +
+				`authors: ${book.authors}\n` +
+				`publisher: ${book.publisher}\n` +
+				`publishedDate: ${book.publishedDate}\n` +
+				`pageCount: ${book.pageCount}\n` +
+				`textSnippet: ${book.textSnippet}\n` + 
+				`description: ${book.description}\n` +
+				`language: ${book.language}\n` +
+				`location: ${book.location}\n` +
+				`thumbnail: ${book.thumbnail}\n`;
+			if ((total % batchSize === 0) || (i === results.length - 1)) {
+				await sendMessage(env, chatId, message);
+				await new Promise(resolve => setTimeout(resolve, 30));
+				message = ""; 
+			}
+		}
+	} else await sendMessage(env, chatId, "You must provide a valid ISBN10 or ISBN13.");
+}
+
+/** 
+ * Count all books in the DB.
+ * 
+ * @param {object} env - The environment object containing runtime information, such as bindings.
+ * @param {number|string} chatId - The chat ID of the user who requested the service.
+ * @returns {Promise<void>} This function does not return a value.
+ */
+async function countBooks(env, chatId) {
+	const { results } = await env.db.prepare(`SELECT COUNT(*) AS tot FROM books`).all();
+	await sendMessage(env, chatId, `Total books: ${results[0]["tot"]}.`);
+}
+
+/** 
  * Handles incoming requests to the Cloudflare Worker.
  * 
  * @param {Request} request - The HTTP request object representing the incoming request.
@@ -80,9 +282,9 @@ export default {
 				const chatId = payload.message.chat.id.toString();
 				const text = payload.message.text || "";
 				const command = text.split(" ")[0];
-				const args = message.text.substring(command.length).trim();
+				const args = text.substring(command.length).trim();
 				let edit_command = false;
-				if (chatId in admins) {
+				if (admins.includes(chatId)) {
 					if (command === "/add") {
 						await addBook(env, chatId, args);
 						edit_command = true;
@@ -97,8 +299,10 @@ export default {
 						edit_command = true;
 					} 
 				} 
-				if (chatId in users || (chatId in admins && edit_command === false)) { //remove the users check and make this a else if you want to allow to everyone to see your books
-					if (message.text) await searchBooks(env, chatId, message.text);
+				if (users.includes(chatId) || (admins.includes(chatId) && edit_command === false)) { //remove the users check and make this a else if you want to allow to everyone to see your books
+					if (command === "/show") await showBook(env, chatId, args);
+					else if (command === "/count") await countBooks(env, chatId);
+					else if (text) await searchBooks(env, chatId, text);
 					else await sendMessage(env, chatId, "Incorrect usage, check /help.");
 				}
 				else {
@@ -109,55 +313,3 @@ export default {
     return new Response("OK", { status: 200 });
   },
 };
-
-async function deleteBook(env, chatId, isbn) {
-    const book = await env.BOOKS.get(isbn);
-    if (!book) return sendMessage(env, chatId, "Libro non trovato.");
-    await env.BOOKS.delete(isbn);
-    return sendMessage(env, chatId, `Libro eliminato: ${JSON.parse(book).title}`);
-}
-
-async function addManually(env, chatId, args) {
-    const [isbn10, isbn13, title, authors, publisher, publishedDate, pageCount, textSnippet, description, language, location] = args.split(";");
-    if (!isbn10 && !isbn13) return sendMessage(env, chatId, "Devi fornire almeno un ISBN.");
-    const book = { isbn10, isbn13, title, authors: authors?.split(","), publisher, publishedDate, pageCount, textSnippet, description, language, location };
-    await env.BOOKS.put(isbn13 || isbn10, JSON.stringify(book));
-    return sendMessage(env, chatId, `Libro aggiunto manualmente: ${title}`);
-}
-
-async function updateBook(env, chatId, command, args) {
-    const [isbn, ...newValue] = args.split(" ");
-    const bookData = await env.BOOKS.get(isbn);
-    if (!bookData) return sendMessage(env, chatId, "Libro non trovato.");
-    const book = JSON.parse(bookData);
-    
-    const fieldMap = {
-        "/settitle": "title",
-        "/setauthors": "authors",
-        "/setpublisher": "publisher",
-        "/setdate": "publishedDate",
-        "/setpages": "pageCount",
-        "/setsnippet": "textSnippet",
-        "/setdesc": "description",
-        "/setlang": "language",
-        "/setlocation": "location"
-    };
-    
-    const field = fieldMap[command];
-    if (!field) return sendMessage(env, chatId, "Comando non valido.");
-    book[field] = field === "authors" ? newValue.join(" ").split(",") : newValue.join(" ");
-    await env.BOOKS.put(isbn, JSON.stringify(book));
-    return sendMessage(env, chatId, `Modificato ${field}: ${newValue.join(" ")}`);
-}
-
-async function searchBooks(env, chatId, title) {
-    const books = await env.BOOKS.list();
-    const results = await Promise.all(books.keys.map(async key => JSON.parse(await env.BOOKS.get(key.name))));
-    const filtered = results.filter(book => book.title.toLowerCase().includes(title.toLowerCase()));
-    
-    if (filtered.length === 0) return sendMessage(env, chatId, "Nessun libro trovato.");
-    return sendMessage(env, chatId, filtered.map(b => `${b.title} di ${b.authors?.join(", ")}`).join("\n"));
-}
-
-
-
